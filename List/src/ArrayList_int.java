533672ac95ef177f7e3d65dcca24dbd296943d93
Gamika Jayawardana <gamikakj@gmail.com>
2024-08-24 11:38:20 +0530

    update

---
+++ List/src/ArrayList_int.java
@@ -0,0 +1,28 @@
+public class ArrayList_int {
+
+    int maxSize;
+    int position;
+    int[] listEntry;
+
+    public ArrayList_int(int size) {
+        maxSize = size;
+        position = -1;
+        listEntry = new int[maxSize];
+    }
+
+    boolean isListEmpty (){
+        return ( position == -1);
+    }
+
+    boolean isListFull (){
+        return ( position == maxSize-1 );
+    }
+
+    int listSize() {
+        return (position+1)
+    }
+
+    void insertLast()
+
+
+}
