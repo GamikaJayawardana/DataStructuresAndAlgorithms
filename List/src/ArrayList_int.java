61477fec6e96b5f3de7cdb2944eb063ee09c2ba4
Gamika Jayawardana <gamikakj@gmail.com>
2024-08-24 13:50:06 +0530

    update

--- List/src/ArrayList_int.java
+++ List/src/ArrayList_int.java
@@ -24,13 +24,79 @@ public class ArrayList_int {
 
     void insertLast(int x) {
         if (isListFull()){
-            System.out.println("List is Full");
+            System.out.println("Attempt to insert at the end of a full list");
         }
         position++;
         listEntry[position] = x;
     }
 
+    void insertList(int p, int element ) {
+        if (isListFull()) {
+            System.out.println("Attempt to insert an entry into a full list");
+        }
+        else if ( p < 0 || p > (listSize())) {
+            System.out.println("attempt to insert a position not in the list");
+        }
+        else {
+            for (int i = (listSize()); i < p ; i--) {
+                listEntry[i] = listEntry[i-1];
+            }
+            listEntry[p] = element;
+            position++;
 
+        }
 
+    }
+
+    int deleteList(int p) {
+        if (isListEmpty()) {
+            System.out.println("Attempt to delete an entry from an empty list");
+        }
+        else if (p < 0 || p > (listSize())) {
+            System.out.println("attempt to delete a position not in the list");
+        }
+        else {
+            int element = listEntry[p];
+            for (int i = p; i < (listSize()) ; i++) {
+                listEntry[i] = listEntry[i=1];
+            }
+            position--;
+            return element;
+        }
+        return 0;
+    }
+
+    int retriveList(int p) {
+        if (isListEmpty()) {
+            System.out.println("Attempt to delete an entry from an empty list");
+        }
+        else if (p < 0 || p > (listSize())) {
+            System.out.println("attempt to delete a position not in the list");
+        }
+        else {
+            return listEntry[p];
+        }
+        return 0;
+    }
+
+    int replaceList(int p, int x) {
+        if (isListEmpty()) {
+            System.out.println("Attempt to delete an entry from an empty list");
+        }
+        else if (p < 0 || p > (listSize())) {
+            System.out.println("attempt to delete a position not in the list");
+        }
+        else {
+            listEntry[p] = x;
+        }
+        return 0;
+    }
+
+    void traverselList() {
+        int i;
+        for (i=0; i< position+1; i++) {
+            System.out.println(listEntry[i]);
+        }
+    }
 
 }
