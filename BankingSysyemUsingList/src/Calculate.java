b036336c386a75b2025ef495ad32778bec0a43fc
Gamika Jayawardana <gamikakj@gmail.com>
2024-08-25 22:44:22 +0530

    update

--- BankingSysyemUsingList/src/Calculate.java
+++ BankingSysyemUsingList/src/Calculate.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Calculate {
@@ -6,9 +7,25 @@ public class Calculate {
     public static void main(String[] args) {
 
         loaddata();
+        System.out.println("Sorted customer details according to Total Account Balance\n");
         QuickSort sortedlist = new QuickSort();
         sortedlist.quick(list,0,list.maxSize-1);
         list.traverselList();
+
+        displayCustomerAbove(1000000);
+
+
+    }
+
+    private static void displayCustomerAbove(int above) {
+        System.out.println("Customers who have more than 1 000 000 in their account\n");
+        System.out.printf("%-20s %-20s %-20s %20s %20s %20s%n","Acc No","Customer Name","Acc Type","Acc Balance","Interest","Total Balance");
+        for (int i = 0; i < list.listSize(); i++) {
+            if (list.listEntry[i].accBalance > above){
+                list.retriveList(i);
+                System.out.printf("%-20s %-20s %-20s %20s %20s %20s%n",list.listEntry[i].accNumber,list.listEntry[i].cusName,list.listEntry[i].accType,list.listEntry[i].accBalance,list.listEntry[i].interest,list.listEntry[i].totalBalance);
+            }
+        }
     }
 
     private static void loaddata() {
